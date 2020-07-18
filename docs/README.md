d798ab018374ba22ad5c39726195a4f5f1accb6c
YannisZa <yzachos@gmail.com>
2020-07-19 00:51:13 +0300

    updated docs

--- docs/README.md
+++ docs/README.md
@@ -1,6 +1,17 @@
 # Stochastic modelling of urban travel demand
 
-Before running any python script please run ```chmod +x ./setup.sh ``` and then ``` ./setup.sh ```. 
+[![Build Status](http://img.shields.io/travis/badges/badgerbadgerbadger.svg?style=flat-square)](https://travis-ci.org/badges/badgerbadgerbadger)
+[![License](http://img.shields.io/:license-mit-blue.svg?style=flat-square)](http://badges.mit-license.org)
 
-This bash script installs python requirements, sets up necessary directories, and runs relevant tests.
+Before running any python script please run ```chmod +x ./setup.sh ``` and then ``` ./setup.sh ```. This bash script installs python requirements, sets up necessary directories etc.
 
+My thesis will be released soon on my [website][https://yannisza.github.io/].
+
+This repository is a work in progress. The following tasks have to be completed
+- [] Make doubly constrained spatial interaction model class compatible to the singly constrained model class.
+- [] Complete `generate_figures.sh` script.
+- [] Improve comments on model classes and argsparse descriptions in inference classes.
+- [] Write tests for all classes.
+- [] Release outputs in nice format.
+
+Give the repository a star and fork it if you like it!
--- generate_figures.sh
+++ generate_figures.sh
@@ -8,61 +8,8 @@ trap 'last_command=$current_command; current_command=$BASH_COMMAND' DEBUG
 figure_number="$1"
 dataset="$2"
 mode="$3" # s for stochastic and d for deterministic
+cost_matrix_type="$4" # sn for small network and 'ed' for naive Euclidean distance based
 
-
-# if [[ $figure_number = "4a" ]] || [[ -z $figure_number ]]; then
-#   echo "-------------Generating data and plots for potential grid search estimation-------------"
-#
-#   if [[ $dataset = "synthetic" ]] || [[ -z $dataset ]]; then
-#     echo "$dataset = synthetic"
-#     if [[ $mode = "d" ]] || [[ -z $mode ]]; then
-#       echo "mode = deterministic"
-#       python visualisation/potential_analysis.py -data synthetic -d 0 -bmax 2800000 -amin -2 -n 100 -hide
-#     fi
-#     if [[ $mode = "s" ]] || [[ -z $mode ]]; then
-#       echo "mode = stochastic"
-#       python visualisation/potential_analysis.py -data synthetic -d 0.00617494255748621 -bmax 10 -amax 0.2 -n 100 -hide
-#     fi
-#   fi
-#
-#   if [[ $dataset = "retail" ]] || [[ -z $dataset ]]; then
-#     echo "$dataset = retail"
-#     if [[ $mode = "d" ]] || [[ -z $mode ]]; then
-#       echo "mode = deterministic"
-#       python visualisation/potential_analysis.py -data retail -d 0 -bmax 1400000 -n 1000 -hide
-#     fi
-#     if [[ $mode = "s" ]] || [[ -z $mode ]]; then
-#       echo "mode = stochastic"
-#       python visualisation/potential_analysis.py -data retail -d 0.00617494255748621 -bmax 1400000 -n 1000 -hide
-#     fi
-#   fi
-#
-#   if [[ $dataset = "commuter_ward" ]] || [[ -z $dataset ]]; then
-#     echo "$dataset = commuter_ward"
-#     if [[ $mode = "d" ]] || [[ -z $mode ]]; then
-#       echo "mode = deterministic"
-#       python visualisation/potential_analysis.py -data commuter_ward -d 0 -bmax 1000000 -n 1000 -hide
-#     fi
-#     if [[ $mode = "s" ]] || [[ -z $mode ]]; then
-#       echo "mode = stochastic"
-#       python visualisation/potential_analysis.py -data commuter_ward -d 0.01178781925343811 -bmax 1000000 -n 1000 -hide
-#     fi
-#   fi
-#
-#   if [[ $dataset = "commuter_borough" ]] || [[ -z $dataset ]]; then
-#     echo "$dataset = commuter_borough"
-#     if [[ $mode = "d" ]] || [[ -z $mode ]]; then
-#       echo "mode = deterministic"
-#       python visualisation/potential_analysis.py -data commuter_borough -d 0 -bmax 1000000 -n 1000 -hide
-#     fi
-#     if [[ $mode = "s" ]] || [[ -z $mode ]]; then
-#       echo "mode = stochastic"
-#       python visualisation/potential_analysis.py -data commuter_borough -d 0.01178781925343811 -bmax 1000000 -n 1000 -hide
-#     fi
-#   fi
-#   echo "----------------------------------------------------------------------------------------------------"
-#   printf "\n"
-# fi
 
 
 if [[ $figure_number = "4b" ]] || [[ -z $figure_number ]]; then
@@ -72,11 +19,25 @@ if [[ $figure_number = "4b" ]] || [[ -z $figure_number ]]; then
     echo "$dataset = synthetic"
     if [[ $mode = "d" ]] || [[ -z $mode ]]; then
       echo "mode = deterministic"
-      python inference/rsquared_analysis.py -data synthetic -d 0 -bmax 1400000 -n 100 -hide
+      if [[ $cost_matrix_type = "eu" ]] || [[ -z $cost_matrix_type ]]; then
+        echo "cost matrix = Euclidean"
+        python inference/rsquared_analysis.py -data synthetic -d 0 -bmax 1400000 -n 100 -hide
+      fi
+      if [[ $cost_matrix_type = "sn" ]] || [[ -z $cost_matrix_type ]]; then
+        echo "cost matrix = Transportation network"
+        python inference/rsquared_analysis.py -data synthetic -d 0 -bmax 1400000 -n 100 -cm sn -hide
+      fi
     fi
     if [[ $mode = "s" ]] || [[ -z $mode ]]; then
       echo "mode = stochastic"
-      python inference/rsquared_analysis.py -data synthetic -d 0.00617494255748621 -bmax 1400000 -n 100 -hide
+      if [[ $cost_matrix_type = "eu" ]] || [[ -z $cost_matrix_type ]]; then
+        echo "cost matrix = Euclidean"
+        python inference/rsquared_analysis.py -data synthetic -d 0.00617494255748621 -bmax 1400000 -n 100 -hide
+      fi
+      if [[ $cost_matrix_type = "sn" ]] || [[ -z $cost_matrix_type ]]; then
+        echo "cost matrix = Transportation network"
+        python inference/rsquared_analysis.py -data synthetic -d 0.00617494255748621 -bmax 1400000 -n 100 -cm sn -hide
+      fi
     fi
   fi
 
@@ -84,11 +45,25 @@ if [[ $figure_number = "4b" ]] || [[ -z $figure_number ]]; then
     echo "$dataset = retail"
     if [[ $mode = "d" ]] || [[ -z $mode ]]; then
       echo "mode = deterministic"
-      python inference/rsquared_analysis.py -data retail -d 0 -bmax 1400000 -n 1000 -hide
+      if [[ $cost_matrix_type = "eu" ]] || [[ -z $cost_matrix_type ]]; then
+        echo "cost matrix = Euclidean"
+        python inference/rsquared_analysis.py -data retail -d 0 -bmax 1400000 -n 1000 -hide
+      fi
+      if [[ $cost_matrix_type = "sn" ]] || [[ -z $cost_matrix_type ]]; then
+        echo "cost matrix = Transportation network"
+        python inference/rsquared_analysis.py -data retail -d 0 -bmax 1400000 -n 1000 -cm sn -hide
+      fi
     fi
     if [[ $mode = "s" ]] || [[ -z $mode ]]; then
       echo "mode = stochastic"
-      python inference/rsquared_analysis.py -data retail -d 0.00617494255748621 -bmax 1400000 -n 1000 -hide
+      if [[ $cost_matrix_type = "eu" ]] || [[ -z $cost_matrix_type ]]; then
+        echo "cost matrix = Euclidean"
+        python inference/rsquared_analysis.py -data retail -d 0.00617494255748621 -bmax 1400000 -n 1000 -hide
+      fi
+      if [[ $cost_matrix_type = "sn" ]] || [[ -z $cost_matrix_type ]]; then
+        echo "cost matrix = Transportation network"
+        python inference/rsquared_analysis.py -data retail -d 0.00617494255748621 -bmax 1400000 -n 1000 -cm sn -hide
+      fi
     fi
   fi
 
@@ -96,11 +71,25 @@ if [[ $figure_number = "4b" ]] || [[ -z $figure_number ]]; then
     echo "$dataset = commuter_ward"
     if [[ $mode = "d" ]] || [[ -z $mode ]]; then
       echo "mode = deterministic"
-      python inference/rsquared_analysis.py -data commuter_ward -d 0 -bmax 1000000 -n 1000 -hide
+      if [[ $cost_matrix_type = "eu" ]] || [[ -z $cost_matrix_type ]]; then
+        echo "cost matrix = Euclidean"
+        python inference/rsquared_analysis.py -data commuter_ward -d 0 -bmax 1000000 -n 1000 -hide
+      fi
+      if [[ $cost_matrix_type = "sn" ]] || [[ -z $cost_matrix_type ]]; then
+        echo "cost matrix = Transportation network"
+        python inference/rsquared_analysis.py -data commuter_ward -d 0 -bmax 1000000 -n 1000 -cm sn -hide
+      fi
     fi
     if [[ $mode = "s" ]] || [[ -z $mode ]]; then
       echo "mode = stochastic"
-      python inference/rsquared_analysis.py -data commuter_ward -d 0.01178781925343811 -bmax 1000000 -n 1000 -hide
+      if [[ $cost_matrix_type = "eu" ]] || [[ -z $cost_matrix_type ]]; then
+        echo "cost matrix = Euclidean"
+        python inference/rsquared_analysis.py -data commuter_ward -d 0.01178781925343811 -bmax 1000000 -n 1000 -hide
+      fi
+      if [[ $cost_matrix_type = "sn" ]] || [[ -z $cost_matrix_type ]]; then
+        echo "cost matrix = Transportation network"
+        python inference/rsquared_analysis.py -data commuter_ward -d 0.01178781925343811 -bmax 1000000 -n 1000 -cm sn -hide
+      fi
     fi
   fi
 
@@ -108,11 +97,25 @@ if [[ $figure_number = "4b" ]] || [[ -z $figure_number ]]; then
     echo "$dataset = commuter_borough"
     if [[ $mode = "d" ]] || [[ -z $mode ]]; then
       echo "mode = deterministic"
-      python inference/rsquared_analysis.py -data commuter_borough -d 0 -bmax 1000000 -n 1000 -hide
+      if [[ $cost_matrix_type = "eu" ]] || [[ -z $cost_matrix_type ]]; then
+        echo "cost matrix = Euclidean"
+        python inference/rsquared_analysis.py -data commuter_borough -d 0 -bmax 1000000 -n 1000 -hide
+      fi
+      if [[ $cost_matrix_type = "sn" ]] || [[ -z $cost_matrix_type ]]; then
+        echo "cost matrix = Transportation network"
+        python inference/rsquared_analysis.py -data commuter_borough -d 0 -bmax 1000000 -n 1000 -cm sn -hide
+      fi
     fi
     if [[ $mode = "s" ]] || [[ -z $mode ]]; then
       echo "mode = stochastic"
-      python inference/rsquared_analysis.py -data commuter_borough -d 0.01178781925343811 -bmax 1000000 -n 1000 -hide
+      if [[ $cost_matrix_type = "eu" ]] || [[ -z $cost_matrix_type ]]; then
+        echo "cost matrix = Euclidean"
+        python inference/rsquared_analysis.py -data commuter_borough -d 0.01178781925343811 -bmax 1000000 -n 1000 -hide
+      fi
+      if [[ $cost_matrix_type = "sn" ]] || [[ -z $cost_matrix_type ]]; then
+        echo "cost matrix = Transportation network"
+        python inference/rsquared_analysis.py -data commuter_borough -d 0.01178781925343811 -bmax 1000000 -n 1000 -cm sn -hide
+      fi
     fi
   fi
   echo "----------------------------------------------------------------------------------------------------"
@@ -126,27 +129,52 @@ if [[ $figure_number = "4c" ]] || [[ -z $figure_number ]]; then
     echo "$dataset = retail"
     if [[ $mode = "d" ]] || [[ -z $mode ]]; then
       echo "mode = deterministic"
-      python inference/laplace_analysis.py -data retail -d 0.00617494255748621 -bmax 1400000 -g 100 -hide
+      if [[ $cost_matrix_type = "eu" ]] || [[ -z $cost_matrix_type ]]; then
+        echo "cost matrix = Euclidean"
+        python inference/laplace_analysis.py -data retail -d 0.00617494255748621 -bmax 1400000 -g 100 -hide
+      fi
+      if [[ $cost_matrix_type = "sn" ]] || [[ -z $cost_matrix_type ]]; then
+        echo "cost matrix = Transportation network"
+        python inference/laplace_analysis.py -data retail -d 0.00617494255748621 -bmax 1400000 -g 100 -cm sn -hide
+      fi
     fi
     if [[ $mode = "s" ]] || [[ -z $mode ]]; then
       echo "mode = stochastic"
-      python inference/laplace_analysis.py -data retail -d 0.00617494255748621 -bmax 1400000 -g 10000 -hide
+      if [[ $cost_matrix_type = "eu" ]] || [[ -z $cost_matrix_type ]]; then
+        echo "cost matrix = Euclidean"
+        python inference/laplace_analysis.py -data retail -d 0.00617494255748621 -bmax 1400000 -g 10000 -hide
+      fi
+      if [[ $cost_matrix_type = "sn" ]] || [[ -z $cost_matrix_type ]]; then
+        echo "cost matrix = Transportation network"
+        python inference/laplace_analysis.py -data retail -d 0.00617494255748621 -bmax 1400000 -g 10000 -cm sn -hide
+      fi
     fi
   fi
-
-  # python inference/laplace_analysis.py -data synthetic -d 0.2727272727272727 -bmax 12 -g 100
-  # python inference/laplace_analysis.py -data synthetic -d 0.2727272727272727 -bmax 12 -g 10000
 
 
   if [[ $dataset = "commuter_ward" ]] || [[ -z $dataset ]]; then
     echo "$dataset = commuter_ward"
     if [[ $mode = "d" ]] || [[ -z $mode ]]; then
       echo "mode = deterministic"
-      python inference/laplace_analysis.py -data commuter_ward -d 0.01178781925343811 -bmax 1400000 -g 10000 -hide # or bmax 71260
+      if [[ $cost_matrix_type = "eu" ]] || [[ -z $cost_matrix_type ]]; then
+        echo "cost matrix = Euclidean"
+        python inference/laplace_analysis.py -data commuter_ward -d 0.01178781925343811 -bmax 1400000 -g 10000 -hide # or bmax 71260
+      fi
+      if [[ $cost_matrix_type = "sn" ]] || [[ -z $cost_matrix_type ]]; then
+        echo "cost matrix = Transportation network"
+        python inference/laplace_analysis.py -data commuter_ward -d 0.01178781925343811 -bmax 1400000 -g 10000 -cm sn -hide # or bmax 71260
+      fi
     fi
     if [[ $mode = "s" ]] || [[ -z $mode ]]; then
       echo "mode = stochastic"
-      python inference/laplace_analysis.py -data commuter_ward -d 0.01178781925343811 -bmax 1400000 -g 100 -hide# or bmax 71260
+      if [[ $cost_matrix_type = "eu" ]] || [[ -z $cost_matrix_type ]]; then
+        echo "cost matrix = Euclidean"
+        python inference/laplace_analysis.py -data commuter_ward -d 0.01178781925343811 -bmax 1400000 -g 100 -hide# or bmax 71260
+      fi
+      if [[ $cost_matrix_type = "sn" ]] || [[ -z $cost_matrix_type ]]; then
+        echo "cost matrix = Transportation network"
+        python inference/laplace_analysis.py -data commuter_ward -d 0.01178781925343811 -bmax 1400000 -g 100 -hide# or bmax 71260
+      fi
     fi
   fi
 
@@ -154,11 +182,25 @@ if [[ $figure_number = "4c" ]] || [[ -z $figure_number ]]; then
     echo "$dataset = commuter_borough"
     if [[ $mode = "d" ]] || [[ -z $mode ]]; then
       echo "mode = deterministic"
-      python inference/laplace_analysis.py -data commuter_borough -d 0.01178781925343811 -bmax 1400000 -g 10000 -hide # or bmax 71260
+      if [[ $cost_matrix_type = "eu" ]] || [[ -z $cost_matrix_type ]]; then
+        echo "cost matrix = Euclidean"
+        python inference/laplace_analysis.py -data commuter_borough -d 0.01178781925343811 -bmax 1400000 -g 10000 -hide # or bmax 71260
+      fi
+      if [[ $cost_matrix_type = "sn" ]] || [[ -z $cost_matrix_type ]]; then
+        echo "cost matrix = Transportation network"
+        python inference/laplace_analysis.py -data commuter_borough -d 0.01178781925343811 -bmax 1400000 -g 10000 -hide # or bmax 71260
+      fi
     fi
     if [[ $mode = "s" ]] || [[ -z $mode ]]; then
       echo "mode = stochastic"
-      python inference/laplace_analysis.py -data commuter_borough -d 0.01178781925343811 -bmax 1400000 -g 100 -hide # or bmax 71260
+      if [[ $cost_matrix_type = "eu" ]] || [[ -z $cost_matrix_type ]]; then
+        echo "cost matrix = Euclidean"
+        python inference/laplace_analysis.py -data commuter_borough -d 0.01178781925343811 -bmax 1400000 -g 100 -hide # or bmax 71260
+      fi
+      if [[ $cost_matrix_type = "sn" ]] || [[ -z $cost_matrix_type ]]; then
+        echo "cost matrix = Transportation network"
+        python inference/laplace_analysis.py -data commuter_borough -d 0.01178781925343811 -bmax 1400000 -g 100 -hide # or bmax 71260
+      fi
     fi
   fi
 
